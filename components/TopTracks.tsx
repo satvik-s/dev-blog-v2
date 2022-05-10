06fadeb6beafa1682f981d49b41040c40eb9ed7b
Satvik Shukla <satvik.shukla@outlook.com>
2022-05-09 18:37:10 -0700

    try static rendering

--- components/TopTracks.tsx
+++ components/TopTracks.tsx
@@ -3,19 +3,37 @@ import useSWR from 'swr';
 import fetcher from 'lib/fetcher';
 import { TopTracks } from 'lib/types';
 import Track from 'components/Track';
+import { getTopTracks } from 'lib/spotify';
 
-export default function Tracks() {
+export default function Tracks({ _data }) {
   const { data } = useSWR<TopTracks>('/api/top-tracks', fetcher);
 
-  if (!data) {
+  if (!data && !_data) {
     return null;
   }
 
   return (
     <>
-      {data.tracks.map((track, index) => (
+      {(data ?? _data).tracks.map((track, index) => (
         <Track ranking={index + 1} key={track.songUrl} {...track} />
       ))}
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await getTopTracks();
+  const items = await res.json();
+
+  const tracks = items.map((item) => ({
+    artist: item.artists,
+    songUrl: item.track_url,
+    title: item.track_name,
+  }));
+
+  return {
+    props: {
+      data: { tracks },
+    },
+  };
 }
