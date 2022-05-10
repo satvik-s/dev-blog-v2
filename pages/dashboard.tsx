547e6f410bbf0fb5caa00128c37f61755ddf11b2
Satvik Shukla <61213829+satvik-s@users.noreply.github.com>
2022-05-09 21:47:26 -0700

    feat: try static rendering (#34)
    * try static rendering
    
    * fix build
    
    * try static gen
    
    * update
    
    * revert
    
    * try again
    
    * fix
    
    * nit

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,13 @@
 import Container from 'components/Container';
 import CurrentPlayingDetailed from 'components/CurrentPlayingDetailed';
 import TopTracks from 'components/TopTracks';
+import { getTopTracks } from 'lib/spotify';
 
-export default function Dashboard() {
+export default function Dashboard({ data }) {
   return (
     <Container
       title="Dashboard – Satvik Shukla"
-      description="My personal dashboard, built with Next.js API routes deployed as serverless functions."
+      description="My personal dashboard, built with Next.js API routes deployed as serverless functions and some static rendering."
     >
       <div className="flex flex-col justify-center items-start max-w-2xl w-full mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
@@ -15,7 +16,7 @@ export default function Dashboard() {
         <div className="mb-8">
           <p className="text-gray-600 dark:text-gray-400 mb-4">
             This is my personal dashboard, built with Next.js API routes
-            deployed as serverless functions.
+            deployed as serverless functions and some static rendering.
           </p>
         </div>
         <CurrentPlayingDetailed />
@@ -26,8 +27,28 @@ export default function Dashboard() {
           Curious what I'm currently listening to? Here's my top tracks on
           Spotify from the past month
         </p>
-        <TopTracks />
+        <TopTracks _data={data} />
       </div>
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const response = await getTopTracks();
+
+  const items = await response.json();
+
+  const tracks = items.map((item) => ({
+    artist: item.artists,
+    songUrl: item.track_url,
+    title: item.track_name,
+  }));
+
+  return {
+    props: {
+      data: {
+        tracks,
+      },
+    },
+  };
 }
