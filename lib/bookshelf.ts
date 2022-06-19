a42afbb9a30725bf93333907e87a8d35de873947
Satvik Shukla <satvik.shukla@outlook.com>
2022-06-18 18:40:19 -0700

    nit

--- lib/bookshelf.ts
+++ lib/bookshelf.ts
@@ -1,5 +1,5 @@
 const BOOKSHELF_INFO_ENDPOINT =
-  'https://jb2h6gutiqmt75wn2ih6r2llme0uuvja.lambda-url.us-west-2.on.aws/';
+  'https://jb2h6gutiqmt75wn2ih6r2llme0uuvja.lambda-url.us-west-2.on.aws';
 
 export const getBookshelfInfo = async () => {
   return await fetch(BOOKSHELF_INFO_ENDPOINT);
