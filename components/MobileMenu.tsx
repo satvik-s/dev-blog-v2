6729b9b056b73dc19230a18771f80927017d4dd2
Satvik Shukla <satvik.shukla@outlook.com>
2022-07-25 12:36:45 -0700

    update deps

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,7 +1,6 @@
 import cn from 'classnames';
 import Link from 'next/link';
-import useDelayedRender from 'use-delayed-render';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import styles from 'styles/mobile-menu.module.css';
 
 export default function MobileMenu() {
@@ -134,4 +133,72 @@ function CrossIcon(props: JSX.IntrinsicElements['svg']) {
       <path d="M6 6l12 12" />
     </svg>
   );
+}
+
+interface Options {
+  enterDelay?: number;
+  exitDelay?: number;
+  onUnmount?: () => void;
+}
+
+function useDelayedRender(active: boolean = false, options: Options = {}) {
+  const [, force] = useState<any>();
+  const mounted = useRef(active);
+  const rendered = useRef(false);
+  const renderTimer = useRef<NodeJS.Timeout | null>(null);
+  const unmountTimer = useRef<NodeJS.Timeout | null>(null);
+  const prevActive = useRef(active);
+
+  const recalculate = useCallback(() => {
+    const { enterDelay = 1, exitDelay = 0 } = options;
+
+    if (prevActive.current) {
+      // Mount immediately
+      mounted.current = true;
+      if (unmountTimer.current) clearTimeout(unmountTimer.current);
+
+      if (enterDelay <= 0) {
+        // Render immediately
+        rendered.current = true;
+      } else {
+        if (renderTimer.current) return;
+
+        // Render after a delay
+        renderTimer.current = setTimeout(() => {
+          rendered.current = true;
+          renderTimer.current = null;
+          force({});
+        }, enterDelay);
+      }
+    } else {
+      // Immediately set to unrendered
+      rendered.current = false;
+
+      if (exitDelay <= 0) {
+        mounted.current = false;
+      } else {
+        if (unmountTimer.current) return;
+
+        // Unmount after a delay
+        unmountTimer.current = setTimeout(() => {
+          mounted.current = false;
+          unmountTimer.current = null;
+          force({});
+        }, exitDelay);
+      }
+    }
+  }, [options]);
+
+  // When the active prop changes, need to re-calculate
+  if (active !== prevActive.current) {
+    prevActive.current = active;
+    // We want to do this synchronously with the render, not in an effect
+    // this way when active → true, mounted → true in the same pass
+    recalculate();
+  }
+
+  return {
+    mounted: mounted.current,
+    rendered: rendered.current,
+  };
 }
